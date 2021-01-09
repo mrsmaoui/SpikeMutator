d7acaa11ac80e28a05f54ed24ded4cf69fce3839
mrsmaoui <mohamed.smaoui@mail.mcgill.ca>
2021-01-10 01:17:46 +0300

    adding simulation operations for open spike state

--- SpikeMutator.py
+++ SpikeMutator.py
@@ -12,8 +12,10 @@ from subprocess import call
 import string
 import sys 
 import AquasolCPLX_routine
-from spike_down import mapRealToModel
-import spike_up
+from spike_closed import mapRealToModel
+from spike_open import mapRealToModelOpenA
+from spike_open import mapRealToModelOpenB
+from spike_open import mapRealToModelOpenC
 import time
 import os
 
@@ -37,8 +39,19 @@ def main(argv):
 	conformation = ''
 	action = 'mutation'
 
-	fasta = open("spike.fasta", "r")
+	# Closed structure
+	fasta = open("spikes/closed/spike.fasta", "r")
 	sequence = fasta.readline().strip()
+
+	# Open structure
+	fasta = open("spikes/open/spike_A_open.fasta", "r")
+	sequence_A_open = fasta.readline().strip()
+
+	fasta = open("spikes/open/spike_B_open.fasta", "r")
+	sequence_B_open = fasta.readline().strip()
+
+	fasta = open("spikes/open/spike_C_open.fasta", "r")
+	sequence_C_open = fasta.readline().strip()
 
 	try:
 		for i in range(len(argv)):
@@ -54,9 +67,9 @@ def main(argv):
 
 	#check that input file is of pdb format
 
-	if(sequence == ""):
+	if(sequence == "" or sequence_A_open == "" or sequence_B_open == "" or sequence_C_open == ""):
 		print error
-		print "fasta file did not load properly"
+		print "Fasta file(s) did not load properly. Make sure they exist in the spikes/ directory"
 		sys.exit(1)
 
 	if(mutation != "" and mutation != "all" and ',' not in mutation):
@@ -67,9 +80,9 @@ def main(argv):
 			print "ERROR: Incorrect mutation choice"
 			sys.exit(0)	
 
-	if "down" not in conformation and "up" not in conformation:
+	if "closed" not in conformation and "open" not in conformation:
 		print error
-		print "Kindly specify a conformation to use for the spike by using the -c flag: -c down, or -c up"
+		print "Kindly specify a conformation to use for the spike by using the -c flag: -c closed, or -c open"
 		sys.exit(1)
 
 	if "energy" not in action and "mutation" not in action:
@@ -79,13 +92,15 @@ def main(argv):
 
 	#Determine which USAGE the user intends to perform
 	if( (position != '') and (position != 'all') and ',' not in position and int(position) > 0 and len(mutation) == 1):
-		params = {"filename":filename, "position":position, "mutation":mutation, "sequence":sequence, "offset":offset, "action":action}  	
 		
-		if conformation == "up":
-			return mutateUp(params)
+		if conformation == "open":
+			params = {"filename":filename, "position":position, "mutation":mutation, 
+				"sequence_A":sequence_A_open, "sequence_B":sequence_B_open, "sequence_C":sequence_C_open, "offset":offset, "action":action}  	
+			return mutateOpen(params)
 
-		if conformation == "down":
-			return mutateDown(params)
+		if conformation == "closed":
+			params = {"filename":filename, "position":position, "mutation":mutation, "sequence":sequence, "offset":offset, "action":action}  	
+			return mutateClosed(params)
 
 	else:
 		print error
@@ -126,7 +141,7 @@ def mutateSequence(x, m, sequence, offset):
 	return "".join(originalSeq)	
 	
 
-def mutateDown(params):
+def mutateClosed(params):
 
 	filename = params['filename']
 	mutation = params['mutation']
@@ -144,35 +159,42 @@ def mutateDown(params):
 	#Perform Mutation on original sequence
 	mutant_seq = mutateSequence(model_pos, mutation, sequence, offset)
 	
-	print(simulate(mutation, mutant_seq, position, filename, action))
+	print(simulateClosed(mutation, mutant_seq, position, filename, action))
 
 	os.chdir(SPIKE_PATH)
 	
 
-def mutateUp(params):
+def mutateOpen(params):
 
 	filename = params['filename']
 	mutation = params['mutation']
 	position = int(params['position'])
-	sequence = params['sequence']
+	sequence_A_open = params['sequence_A']
+	sequence_B_open = params['sequence_B']
+	sequence_C_open = params['sequence_C']
 	offset = params['offset']
 	action = params['action']
 	
-	# Check if position is available in the 6VXX structure
-	model_pos = mapRealToModel[position]
-	if model_pos == 0:
-		print("The selected position (%d) cannot be tested because it does not exist in the 6VXX protein structure" % model_pos)
+	# Check if position is available in the 6VYB structure
+	model_posA = mapRealToModelOpenA[position]
+	model_posB = mapRealToModelOpenB[position]
+	model_posC = mapRealToModelOpenC[position]
+
+	if model_posA == 0 or model_posB == 0 or model_posC == 0:
+		print("The selected position (%d) cannot be tested because it does not exist in one or more of the the 6VYB protein chain structures" % model_pos)
 		exit()
 
 	#Perform Mutation on original sequence
-	mutant_seq = mutateSequence(model_pos, mutation, sequence, offset)
+	mutant_seq_A = mutateSequence(model_pos, mutation, sequence_A_open, offset)
+	mutant_seq_B = mutateSequence(model_pos, mutation, sequence_B_open, offset)
+	mutant_seq_C = mutateSequence(model_pos, mutation, sequence_C_open, offset)
 	
-	print(simulate(mutation, mutant_seq, position, filename, action))
+	print(simulateOpen(mutation, mutant_seq_A, mutant_seq_B, mutant_seq_C, position, filename, action))
 
 	os.chdir(SPIKE_PATH)
 
 
-def simulate(mutation, sequence, position, filename, action):
+def simulateClosed(mutation, sequence, position, filename, action):
 	os.chdir(SPIKE_PATH)	
 	timestr = time.strftime("%Y%m%d-%H%M%S")
 	usage_path = SPIKE_PATH + '/tmp/' + timestr
@@ -186,11 +208,11 @@ def simulate(mutation, sequence, position, filename, action):
 	
 	#Perform mutation and generate structure file
 	
-	call("%s/Scwrl4 -i %s/spike_A.pdb -o %s/scwrl_spike_A_%d-%s.pdb -s %s/%s_%d-%s.fasta >/dev/null" % (SCWRL4_PATH, SPIKE_PATH, usage_path, position, mutation, usage_path, filename, position, mutation), shell=True)
+	call("%s/Scwrl4 -i %s/spikes/closed/spike_A.pdb -o %s/scwrl_spike_A_%d-%s.pdb -s %s/%s_%d-%s.fasta >/dev/null" % (SCWRL4_PATH, SPIKE_PATH, usage_path, position, mutation, usage_path, filename, position, mutation), shell=True)
 
-	call("%s/Scwrl4 -i %s/spike_B.pdb -o %s/scwrl_spike_B_%d-%s.pdb -s %s/%s_%d-%s.fasta >/dev/null" % (SCWRL4_PATH, SPIKE_PATH, usage_path, position, mutation, usage_path, filename, position, mutation), shell=True)
+	call("%s/Scwrl4 -i %s/spikes/closed/spike_B.pdb -o %s/scwrl_spike_B_%d-%s.pdb -s %s/%s_%d-%s.fasta >/dev/null" % (SCWRL4_PATH, SPIKE_PATH, usage_path, position, mutation, usage_path, filename, position, mutation), shell=True)
 
-	call("%s/Scwrl4 -i %s/spike_C.pdb -o %s/scwrl_spike_C_%d-%s.pdb -s %s/%s_%d-%s.fasta >/dev/null" % (SCWRL4_PATH, SPIKE_PATH, usage_path, position, mutation, usage_path, filename, position, mutation), shell=True)
+	call("%s/Scwrl4 -i %s/spikes/closed/spike_C.pdb -o %s/scwrl_spike_C_%d-%s.pdb -s %s/%s_%d-%s.fasta >/dev/null" % (SCWRL4_PATH, SPIKE_PATH, usage_path, position, mutation, usage_path, filename, position, mutation), shell=True)
 
 	call("cat %s/scwrl_spike_A_%d-%s.pdb %s/scwrl_spike_B_%d-%s.pdb %s/scwrl_spike_C_%d-%s.pdb > %s/spike_%d-%s.pdb" % (usage_path, position, mutation, usage_path, position, mutation, usage_path, position, mutation, usage_path, position, mutation), shell=True)
 
@@ -200,7 +222,7 @@ def simulate(mutation, sequence, position, filename, action):
 	
 	resp = ""
 	if action == 'energy':
-                print "Position,Mutation,Coul,LJ,Solvation,Sequence"
+		print "Position,Mutation,Coul,LJ,Solvation,Sequence"
 
 		# Calculate Thermodynamics values
 		(Coul, LJ, Fw, Fv, Nw) = calculateE(usage_path, "spike_%d-%s" % (position, mutation) )
@@ -214,6 +236,50 @@ def simulate(mutation, sequence, position, filename, action):
 
 	return "Mutated Structure created at %s/spike_%d-%s.pdb" % ( (SPIKE_PATH + '/structs'), position, mutation)
 
+
+def simulateOpen(mutation, sequence_A, sequence_B, sequence_C, position, filename, action):
+	os.chdir(SPIKE_PATH)	
+	timestr = time.strftime("%Y%m%d-%H%M%S")
+	usage_path = SPIKE_PATH + '/tmp/' + timestr
+
+	#Create a workspace directory
+	call('mkdir %s' % usage_path, shell=True)
+	call('mkdir %s/trash' % usage_path, shell=True)
+
+	#Generate .fasta file 
+	call('echo %s > %s/%s_A_%d-%s.fasta' % (sequence_A, usage_path, filename,position,mutation), shell=True)
+	call('echo %s > %s/%s_B_%d-%s.fasta' % (sequence_B, usage_path, filename,position,mutation), shell=True)
+	call('echo %s > %s/%s_C_%d-%s.fasta' % (sequence_C, usage_path, filename,position,mutation), shell=True)
+	
+	#Perform mutation and generate structure file
+	
+	call("%s/Scwrl4 -i %s/spikes/open/spike_A_open.pdb -o %s/scwrl_spike_A_%d-%s.pdb -s %s/%s_A_%d-%s.fasta >/dev/null" % (SCWRL4_PATH, SPIKE_PATH, usage_path, position, mutation, usage_path, filename, position, mutation), shell=True)
+
+	call("%s/Scwrl4 -i %s/spikes/open/spike_B_open.pdb -o %s/scwrl_spike_B_%d-%s.pdb -s %s/%s_B_%d-%s.fasta >/dev/null" % (SCWRL4_PATH, SPIKE_PATH, usage_path, position, mutation, usage_path, filename, position, mutation), shell=True)
+
+	call("%s/Scwrl4 -i %s/spikes/open/spike_C_open.pdb -o %s/scwrl_spike_C_%d-%s.pdb -s %s/%s_C_%d-%s.fasta >/dev/null" % (SCWRL4_PATH, SPIKE_PATH, usage_path, position, mutation, usage_path, filename, position, mutation), shell=True)
+
+	call("cat %s/scwrl_spike_A_%d-%s.pdb %s/scwrl_spike_B_%d-%s.pdb %s/scwrl_spike_C_%d-%s.pdb > %s/spike_%d-%s.pdb" % (usage_path, position, mutation, usage_path, position, mutation, usage_path, position, mutation, usage_path, position, mutation), shell=True)
+
+
+	# Copy structure file into runs/ directory
+	call("cp %s/spike_%d-%s.pdb %s/" % (usage_path, position, mutation, (SPIKE_PATH + '/structs/')), shell=True)
+	
+	resp = ""
+	if action == 'energy':
+		print "Position,Mutation,Coul,LJ,Solvation,Sequence"
+
+		# Calculate Thermodynamics values
+		(Coul, LJ, Fw, Fv, Nw) = calculateE(usage_path, "spike_%d-%s" % (position, mutation) )
+		solvation = float(Fw) - float(Fv) - (float(Nw) * -0.0352669760297406)
+		resp = str(position) + "," + mutation + "," + str(Coul) + "," + str(LJ) + "," + str(solvation) + "," + sequence
+
+		#remove directory
+		call('rm -rf %s/tmp/%s' % (SPIKE_PATH, timestr), shell=True)
+	
+		return resp
+
+	return "Mutated Structure created at %s/spike_%d-%s.pdb" % ( (SPIKE_PATH + '/structs'), position, mutation)
 
 
 main(sys.argv)
